f92550a59932d615d40138c409686f512511c408
Niels van Schooten <nielsvanschooten@gmail.com>
2018-06-11 23:24:15 +0200

    start of this new OS

---
+++ CosKernel/ConsoleParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CosKernel
+{
+    public class ConsoleParser
+    {
+        public string[] KeyStrings = {"|", "&", ">", "<"};
+
+
+        public string ParseConsoleLine(string input)
+        {
+            // check if the console should run the command in the background.
+            if (input.Contains(KeyStrings[1]))
+            {
+                // remove the outer command (if it is the & sign), so we can continue parsing the line!
+                string processLine = input.Substring(0, input.IndexOf(KeyStrings[1], StringComparison.Ordinal) - 1);
+
+                return "PID: " + ProcessManager.Instance.CreateProcess(ParseProcessLine(processLine));
+            }
+
+            return "";
+        }
+
+        private string ParseProcessLine(string substring)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CosKernel/ProcessManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CosKernel
+{
+    public class ProcessManager
+    {
+        private List<SystemProcess> _jobs;
+
+        private static ProcessManager _instance;
+
+        public ProcessManager()
+        {
+            _jobs = new List<SystemProcess>();
+        }
+
+        public static ProcessManager Instance
+        {
+            get { return _instance ?? (_instance = new ProcessManager()); }
+        }
+
+        public SystemProcess GetProcess(string name)
+        {
+            return _jobs.Find(x => x.name == name);
+        }
+
+        public uint CreateProcess(string name)
+        {
+            throw new NotImplementedException("The create process function is not implemented yet because I need to implement the runtimes!!!");
+        }
+    }
+
+    public struct SystemProcess
+    {
+        public int PID;
+        public string name;
+
+    }
+}
